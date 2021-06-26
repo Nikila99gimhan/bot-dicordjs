4ec623cf6d8adad7e91135cc5e86ce7d727a7c59
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-06-26 23:15:53 +0530

    added client method and test some events

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 
 
-const { Client }= require('discord.js');
+const { Client, Message }= require('discord.js');
 const client = new Client();
+
+client.on('ready',() => {
+  console.log(`${client.user.tag} has logged in`); 
+});
+
+client.on('message', (message) => {
+    console.log(`[${message.author.tag}]: ${message.content}`);
+  
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
