45bd7633928bf1226d235bd254ae663d1ec50cda
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-06-29 15:29:37 +0530

    added kick msg

--- src/bot.js
+++ src/bot.js
@@ -21,6 +21,12 @@ client.on('message', (message) => {
     if(CMD_NAME === 'kick'){
       if (args.length === 0) return message.reply('Please provide an ID');
       const member = message.guild.members.cache.get(args[0]);
+      if(member) {
+        member.kick();
+
+      } else {
+        message.channel.send('That member was not found');
+      }
      
 
      console.log(member);
